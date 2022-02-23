b24945b4e5b4c007dd8cad2946833e399b793fdf
Zero <iamvamsi999@gmail.com>
2022-02-24 01:02:14 +0530

    added url for mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/Git-Issue');
+// mongoose.connect('mongodb://localhost/Git-Issue');
+const dbUrl = 'mongodb+srv://UsernameZero:UsernameZero@cluster0.oe1eg.mongodb.net/Git-Issue';
+mongoose.connect( dbUrl , {useNewUrlParser: true});
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting MongoDB"));
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 db.once('open',function(){
     console.log(`Mongo-Db is Connected, the data can be stored now.`);
